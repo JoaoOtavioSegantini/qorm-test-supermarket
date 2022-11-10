661fda98c5310f434b18bf4eb2ea9f3b6ffa6996
João Otávio <joaotavioos@outlook.com>
2022-11-09 21:32:08 -0300

    feat: add public directory ✨

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joaootav/system_supermarket/config"
+	"github.com/joaootav/system_supermarket/config/routes"
 	"github.com/joaootav/system_supermarket/database"
 	"github.com/joho/godotenv"
 )
@@ -20,12 +21,18 @@ func init() {
 }
 
 func main() {
+
 	gin.SetMode(gin.ReleaseMode)
 
 	database.Connect(os.Getenv("DATABASE_URL"))
 	database.Migrate()
 	router := gin.Default()
+	router.Static("/public", "./public")
+	router.StaticFile("favicon.ico", "./public/favicon.ico")
+
 	router.Any("/admin/*resources", gin.WrapH(database.Mux))
-	router.Run(fmt.Sprintf(":%d", config.Config.Port))
+	router.Use(gin.WrapH(routes.Router()))
+
 	log.Printf("Listening on: %v\n", config.Config.Port)
+	router.Run(fmt.Sprintf(":%d", config.Config.Port))
 }
