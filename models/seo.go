20c8fbf9f710267430c7eeb4d91a7fe7f89d4727
João Otávio <joaotavioos@outlook.com>
2022-11-09 21:55:41 -0300

    feat: add seo struct ✨

---
+++ models/seo.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"github.com/qor/l10n"
+	"github.com/qor/seo"
+)
+
+type MySEOSetting struct {
+	seo.QorSEOSetting
+	l10n.Locale
+}
+
+type SEOGlobalSetting struct {
+	SiteName string
+}
+
+var SEOCollection *seo.Collection
