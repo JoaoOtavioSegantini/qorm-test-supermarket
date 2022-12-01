e0a362375f0b53b8868a516a5f57e3b09e6e8caa
João Otávio <joaotavioos@outlook.com>
2022-12-01 08:02:09 -0300

    feat: add home tracer ✨

--- app/home/handlers.go
+++ app/home/handlers.go
@@ -3,6 +3,7 @@ package home
 import (
 	"net/http"
 
+	"github.com/joaootav/system_supermarket/tracer"
 	"github.com/qor/qor"
 	"github.com/qor/qor/utils"
 	"github.com/qor/render"
@@ -15,13 +16,21 @@ type Controller struct {
 
 // Index home index page
 func (ctrl Controller) Index(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+
+	_, home := tracer.Tracer.Start(ctx, "render-home-page")
 	w.WriteHeader(http.StatusOK)
 
 	ctrl.View.Execute("index", map[string]interface{}{}, req, w)
+	home.End()
 }
 
 // SwitchLocale switch locale
 func (ctrl Controller) SwitchLocale(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	_, swlocale := tracer.Tracer.Start(ctx, "switch-locale-language")
+
 	utils.SetCookie(http.Cookie{Name: "locale", Value: req.URL.Query().Get("locale")}, &qor.Context{Request: req, Writer: w})
 	http.Redirect(w, req, req.Referer(), http.StatusSeeOther)
+	swlocale.End()
 }
