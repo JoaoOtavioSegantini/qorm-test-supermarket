fb61e859bdfbab94ecda0533cabbc36f5c7a1740
João Otávio <joaotavioos@outlook.com>
2022-12-01 08:14:12 -0300

    feat: setup tracer ✨

---
+++ tracer/tracer.go
@@ -0,0 +1,20 @@
+package tracer
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joaootav/system_supermarket/infra/opentel"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel/trace"
+)
+
+var Tracer trace.Tracer
+
+func SetupTracer(router *gin.Engine) {
+
+	ot := opentel.NewOpenTel()
+	ot.ServiceName = "GoApp"
+	ot.ServiceVersion = "0.1"
+	ot.ExporterEndpoint = "http://localhost:9411/api/v2/spans"
+	Tracer = ot.GetTracer()
+	router.Use(otelgin.Middleware(ot.ServiceName))
+}
