70cbf65b5b9a3b8ba46cfc3ccf2ed9df283d0285
João Otávio <joaotavioos@outlook.com>
2022-12-01 08:16:37 -0300

    feat: implements zipkin tracer ✨

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/joaootav/system_supermarket/config/routes"
 	"github.com/joaootav/system_supermarket/database"
 	"github.com/joaootav/system_supermarket/database/migrations"
+	"github.com/joaootav/system_supermarket/tracer"
 	"github.com/joho/godotenv"
 )
 
@@ -26,11 +27,15 @@ func main() {
 
 	database.Connect()
 	migrations.AutoMigrate()
-	router := gin.Default()
 
+	router := gin.Default()
+	router.StaticFile("/favicon.ico", "./public/favicon.ico")
 	router.Any("/admin/*resources", gin.WrapH(database.Mux))
 	router.Use(gin.WrapH(routes.SetupRouter()))
 
+	tracer.SetupTracer(router)
+
 	log.Printf("Listening on: %v\n", config.Config.Port)
+
 	router.Run(fmt.Sprintf(":%d", config.Config.Port))
 }
